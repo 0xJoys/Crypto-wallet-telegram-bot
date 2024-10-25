e2fd8be44ad7e506678496d6476f42a64cb288d9
Ajsiefinni Joy <ajisefinnijoy@gmail.com>
2024-10-25 14:21:29 +0100

    Wallet Address Changed to Ton Address

--- src/components/walletAddyBal.tsx
+++ src/components/walletAddyBal.tsx
@@ -11,7 +11,7 @@ const walletAddyBal = () => {
         </div>
         <div className='borde'>
           <div className='flex flex-row items-center gap-1'>
-            <h2>0x649...3009</h2>
+            <h2>1A1zP1eP.....mv7DivfNa</h2>
             <FaCopy />
           </div>
           <div className='text-gray-200 text-lg font-semibold flex gap-2 items-center italic borde'>
